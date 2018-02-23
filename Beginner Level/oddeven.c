f12354f4ea0b3a2483e628ba1b506d8fd44bdaf0
M.gopi anand <anandgopi31@gmail.com>
2018-02-23 11:14:11 +0530

    Update oddeven.c

--- Beginner Level/oddeven.c
+++ Beginner Level/oddeven.c
@@ -4,9 +4,8 @@ int main()
     int number;
 
     printf("Enter an integer: ");
-    scanf("%d", &number);
-
-    // True if the number is perfectly divisible by 2
+    scanf("%d", &number)
+       
     if(number % 2 == 0)
         printf("%d is even.", number);
     else
