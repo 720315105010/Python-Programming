b3c0e053994381b8ba5c817425ee7bcc56419737
M.gopi anand <anandgopi31@gmail.com>
2018-02-23 11:01:08 +0530

    Create vowel.py

---
+++ Beginner Level/vowel.py
@@ -0,0 +1,7 @@
+print("Enter the alphabet")
+if l in ('a', 'e', 'i', 'o', 'u'):
+  print("entered value is a vowel")
+elif l == 'y':
+  print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+else:
+  print("entered value  is a consonant")
