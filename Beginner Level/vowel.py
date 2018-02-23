241f7b253d307bed92e6ee851c286a55516b48dd
M.gopi anand <anandgopi31@gmail.com>
2018-02-23 11:22:47 +0530

    Update vowel.py

--- Beginner Level/vowel.py
+++ Beginner Level/vowel.py
@@ -2,6 +2,6 @@ print("enter the alphabet")
 j=input()
 print(j)
 if j in ["a" or "e" or "i" or "o" or "u"]:
-    print("Entered Alphabet is Vowel")
+     print("Entered Alphabet is Vowel")
 else:
     print("Entered Alphabet is Consonant")
