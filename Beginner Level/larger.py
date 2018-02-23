eb3d90c4844a17e9d50e07aed2200c99e5f53568
M.gopi anand <anandgopi31@gmail.com>
2018-02-23 11:50:00 +0530

    Update larger.py

--- Beginner Level/larger.py
+++ Beginner Level/larger.py
@@ -3,9 +3,9 @@ num1=input()
 num2=input()
 num3=input()
 if (num1 >= num2) and (num1 >= num3):
-   largest = num1
+     largest = num1
 elif (num2 >= num1) and (num2 >= num3):
-   largest = num2
+     largest = num2
 else:
-   largest = num3
+     largest = num3
 print("Largest=",largest)
