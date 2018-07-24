9d221d528cddf8f2b98f55663bf03633f17168db
M.gopi anand <anandgopi31@gmail.com>
2018-07-24 09:48:18 +0530

    Create int.py

---
+++ int.py
@@ -0,0 +1,21 @@
+a=str(input())
+if a=="I":
+ print ("1")
+elif a=="II":
+ print("2")
+elif a=="III":
+ print("3")
+elif a=="IV":
+ print("4")
+elif a=="V":
+ print("5")
+elif a=="VI":
+ print("6")
+elif a=="VII":
+ print("7")
+elif a=="VIII":
+ print("8")
+elif a=="IX":
+ print("9")
+else:
+ print ("error")
